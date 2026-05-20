40a955ab7b4a3416a07a412bb918b5e3909bf2b3
veron <veronika.durn@gmail.com>
2026-05-20 12:04:12 +0200

    Popravek pri poglavju Postopek označevanja ciljne besede v povedih.
    Signed-off-by: veron <veronika.durn@gmail.com>

--- WSI_koda.py
+++ WSI_koda.py
@@ -2,49 +2,23 @@ import torch
 import classla
 from transformers import AutoModel, AutoTokenizer, CamembertForSequenceClassification, CamembertForTokenClassification, CamembertTokenizer, CamembertForMaskedLM, CamembertModel, CamembertForCausalLM, CamembertConfig
 import pandas as pd
-import json
 import random
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from torch.utils.data import DataLoader, TensorDataset
 import torch.optim as optim
+import sys
+import gc
 
 ##############################################################################
 
-# 1. PODATKOVNA MNOŽICA
 podatki = pd.read_csv("testni_dataset.csv", sep=";", encoding="utf-8-sig")
-
-vmesni_slovar = {}
-for _, vrstica in podatki.iterrows():
-    beseda = str(vrstica['Beseda']).lower().strip()
-    pomen_id = vrstica['Pomen_ID']
-    pomen = str(vrstica['Razlaga']).strip()
-    poved = str(vrstica['Poved']).strip()
-    if beseda not in vmesni_slovar:
-        vmesni_slovar[beseda] = {}
-    if pomen not in vmesni_slovar[beseda]:
-        vmesni_slovar[beseda][pomen] = {"pomen_id": pomen_id, "pomenska_razlaga": pomen, "povedi": []}
-    vmesni_slovar[beseda][pomen]["povedi"].append(poved)
-
-sskj_slovar = {}
-for beseda in vmesni_slovar:
-    pomeni = []
-    for pomen in vmesni_slovar[beseda].values():
-        pomeni.append(pomen)
-    pomeni.sort(key=lambda p: p["pomen_id"])
-    sskj_slovar[beseda] = pomeni
-
-##############################################################################
-
-naprava = torch.device("cpu")
-model = r'D:\DigiLing\MAGISTRSKA\Koda\sloberta.2.0.transformers'
-tokenizer = CamembertTokenizer.from_pretrained(model, use_fast=False)
 lematizator = classla.Pipeline('sl', processors='tokenize,pos,lemma')
 
-# 2. PRIPRAVA PODATKOV
+# Postopek označevanja ciljne besede v povedih
 
-def oznaci_ciljno_besedo(poved, ciljna_beseda, pipeline=lematizator):
+def oznaci_besedo_in_preveri_poved(poved, ciljna_beseda, pipeline=lematizator):
     analiza_besedila = pipeline(poved)
     oznacena_poved = []
     najdena_ciljna = False
@@ -59,6 +33,38 @@ def oznaci_ciljno_besedo(poved, ciljna_beseda, pipeline=lematizator):
         return None
     return " ".join(oznacena_poved)
 
+vmesni_slovar = {}
+for _, vrstica in podatki.iterrows():
+    beseda = str(vrstica['Beseda']).lower().strip()
+    pomen_id = vrstica['Pomen_ID']
+    pomen = str(vrstica['Razlaga']).strip()
+    poved = str(vrstica['Poved']).strip()
+    oznacena_poved = oznaci_besedo_in_preveri_poved(poved, beseda)
+    if oznacena_poved is None:
+        continue
+    if beseda not in vmesni_slovar:
+        vmesni_slovar[beseda] = {}
+    if pomen not in vmesni_slovar[beseda]:
+        vmesni_slovar[beseda][pomen] = {"pomen_id": pomen_id, "pomenska_razlaga": pomen, "povedi": []}
+    if oznacena_poved not in vmesni_slovar[beseda][pomen]["povedi"]:
+        vmesni_slovar[beseda][pomen]["povedi"].append(oznacena_poved)
+
+sskj_slovar = {}
+for beseda in vmesni_slovar:
+    pomeni = []
+    for pomen in vmesni_slovar[beseda].values():
+        pomeni.append(pomen)
+    pomeni.sort(key=lambda p: p["pomen_id"])
+    sskj_slovar[beseda] = pomeni
+
+##############################################################################
+
+naprava = torch.device("cpu")
+model = r'D:\DigiLing\MAGISTRSKA\Koda\sloberta.2.0.transformers'
+tokenizer = CamembertTokenizer.from_pretrained(model, use_fast=False)
+
+# Postopek tvorjenja učnih parov povedi
+
 def izberi_razlicen_pomen(podatki, beseda, trenutni_i):
     if beseda not in podatki or len(podatki[beseda]) <= 1:
         return None
@@ -71,7 +77,6 @@ def izberi_razlicen_pomen(podatki, beseda, trenutni_i):
         return None
     izbran_pomen = random.choice(kandidati)
     return random.choice(izbran_pomen["povedi"])
-
 
 def pripravi_ucne_pare(podatki, st_parov_na_pomen=2):
     x_povedi = []  # Seznam parov povedi (npr. "Poved 1 </s> Poved 2")
@@ -87,29 +92,25 @@ def pripravi_ucne_pare(podatki, st_parov_na_pomen=2):
             while ustvarjeni < st_parov_na_pomen and poskusi < 20:
                 poskusi += 1
                 izbrani_par = random.sample(povedi_trenutni_pomen, 2)
-                poved_1 = izbrani_par[0]
-                poved_2_isti = izbrani_par[1]
-                poved_2_razlicen = izberi_razlicen_pomen(podatki, beseda, i)
-                if poved_2_razlicen:
-                    poved_1_oznacena = oznaci_ciljno_besedo(poved_1, beseda)
-                    poved_2_isti_oznacena = oznaci_ciljno_besedo(poved_2_isti, beseda)
-                    poved_2_razlicen_oznacena = oznaci_ciljno_besedo(poved_2_razlicen, beseda)
+                poved_1_oznacena = izbrani_par[0]
+                poved_2_isti_oznacena = izbrani_par[1]
+                poved_2_razlicen_oznacena = izberi_razlicen_pomen(podatki, beseda, i)
 
-                    if poved_1_oznacena and poved_2_isti_oznacena and poved_2_razlicen_oznacena:
-                        # Pozitiven par (isti pomen)
-                        x_povedi.append(f"{poved_1_oznacena} </s> {poved_2_isti_oznacena}")
-                        y_oznake.append(1)
-                        # Negativen par (različen pomen)
-                        x_povedi.append(f"{poved_1_oznacena} </s> {poved_2_razlicen_oznacena}")
-                        y_oznake.append(0)
-                        ustvarjeni += 1
+                if poved_2_razlicen_oznacena is not None:
+                    # Pozitiven par (isti pomen)
+                    x_povedi.append(f"{poved_1_oznacena} </s> {poved_2_isti_oznacena}")
+                    y_oznake.append(1)
+                    # Negativen par (različen pomen)
+                    x_povedi.append(f"{poved_1_oznacena} </s> {poved_2_razlicen_oznacena}")
+                    y_oznake.append(0)
+                    ustvarjeni += 1
 
     x_povedi, y_oznake = shuffle(x_povedi, y_oznake, random_state=42)
     return x_povedi, y_oznake
 
 ##############################################################################
 
-# 3. UČNA (80%) in TESTNA (20%) MNOŽICA
+# Učna in testna množica
 
 x_povedi, y_oznake = pripravi_ucne_pare(sskj_slovar)
 x_train, x_test, y_train, y_test = train_test_split(x_povedi, y_oznake, train_size=0.8, shuffle=True, random_state=42)
@@ -127,6 +128,3 @@ train_labels = torch.tensor(y_train)
 test_labels = torch.tensor(y_test)
 
 ##############################################################################
-
-# 4. UČENJE MODELA
-
